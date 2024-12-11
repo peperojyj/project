dfce82d03bc50df98c973d1464dcd719dc1f14d9
peperojyj <82118996+peperojyj@users.noreply.github.com>
2024-12-11 16:52:16 +0900

    improved little more better

--- train.py
+++ train.py
@@ -1,11 +1,9 @@
 import argparse
 import time
-import os
 import numpy as np
 import torch
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from torch.nn import functional as F
 
 from mscn.util import *
 from mscn.data import get_train_datasets, load_data, make_dataset
@@ -34,31 +32,32 @@ def predict(model, data_loader, cuda):
     t_total = 0.0
 
     model.eval()
-    for batch_idx, data_batch in enumerate(data_loader):
-        samples, predicates, joins, targets, sample_masks, predicate_masks, join_masks = data_batch
+    with torch.no_grad():  # Disable gradient updates during testing
+        for batch_idx, data_batch in enumerate(data_loader):
+            samples, predicates, joins, targets, sample_masks, predicate_masks, join_masks = data_batch
 
-        if cuda:
-            samples, predicates, joins, targets = samples.cuda(), predicates.cuda(), joins.cuda(), targets.cuda()
-            sample_masks, predicate_masks, join_masks = sample_masks.cuda(), predicate_masks.cuda(), join_masks.cuda()
+            if cuda:
+                samples, predicates, joins, targets = samples.cuda(), predicates.cuda(), joins.cuda(), targets.cuda()
+                sample_masks, predicate_masks, join_masks = sample_masks.cuda(), predicate_masks.cuda(), join_masks.cuda()
 
-        samples, predicates, joins, targets = (
-            Variable(samples),
-            Variable(predicates),
-            Variable(joins),
-            Variable(targets),
-        )
-        sample_masks, predicate_masks, join_masks = (
-            Variable(sample_masks),
-            Variable(predicate_masks),
-            Variable(join_masks),
-        )
+            samples, predicates, joins, targets = (
+                Variable(samples),
+                Variable(predicates),
+                Variable(joins),
+                Variable(targets),
+            )
+            sample_masks, predicate_masks, join_masks = (
+                Variable(sample_masks),
+                Variable(predicate_masks),
+                Variable(join_masks),
+            )
 
-        t = time.time()
-        outputs = model(samples, predicates, joins, sample_masks, predicate_masks, join_masks)
-        t_total += time.time() - t
+            t = time.time()
+            outputs = model(samples, predicates, joins, sample_masks, predicate_masks, join_masks)
+            t_total += time.time() - t
 
-        for i in range(outputs.data.shape[0]):
-            preds.append(outputs.data[i])
+            for i in range(outputs.data.shape[0]):
+                preds.append(outputs.data[i])
 
     return preds, t_total
 
@@ -77,12 +76,12 @@ def print_qerror(preds_unnorm, labels_unnorm):
 
     qerror = np.array(qerror, dtype=np.float64)
 
-    print("Median: {}".format(np.median(qerror)))
-    print("90th percentile: {}".format(np.percentile(qerror, 90)))
-    print("95th percentile: {}".format(np.percentile(qerror, 95)))
-    print("99th percentile: {}".format(np.percentile(qerror, 99)))
-    print("Max: {}".format(np.max(qerror)))
-    print("Mean: {}".format(np.mean(qerror)))
+    print("Median: {:.4f}".format(np.median(qerror)))
+    print("90th percentile: {:.4f}".format(np.percentile(qerror, 90)))
+    print("95th percentile: {:.4f}".format(np.percentile(qerror, 95)))
+    print("99th percentile: {:.4f}".format(np.percentile(qerror, 99)))
+    print("Max: {:.4f}".format(np.max(qerror)))
+    print("Mean: {:.4f}".format(np.mean(qerror)))
 
 def train_and_predict(workload_name, num_queries, num_epochs, batch_size, hid_units, cuda):
     num_materialized_samples = 1000
@@ -106,6 +105,7 @@ def train_and_predict(workload_name, num_queries, num_epochs, batch_size, hid_un
     print("Number of training samples: {}".format(len(labels_train)))
     print("Number of validation samples: {}".format(len(labels_test)))
 
+    # Training loop
     model.train()
     for epoch in range(num_epochs):
         loss_total = 0.0
@@ -148,7 +148,30 @@ def train_and_predict(workload_name, num_queries, num_epochs, batch_size, hid_un
 
     print("\nQ-Error validation set:")
     print_qerror(preds_test_unnorm, labels_test_unnorm)
-    print("")
+
+    # Test workload evaluation
+    print("\nEvaluating on workload: {}".format(workload_name))
+    file_name = "workloads/" + workload_name
+    joins, predicates, tables, samples, label = load_data(file_name, num_materialized_samples)
+
+    samples_test = encode_samples(tables, samples, table2vec)
+    predicates_test, joins_test = encode_data(predicates, joins, column_min_max_vals, column2vec, op2vec, join2vec)
+    labels_test, _, _ = normalize_labels(label, min_val, max_val)
+
+    print("Number of test samples: {}".format(len(labels_test)))
+
+    max_num_predicates = max([len(p) for p in predicates_test])
+    max_num_joins = max([len(j) for j in joins_test])
+
+    test_data = make_dataset(samples_test, predicates_test, joins_test, labels_test, max_num_joins, max_num_predicates)
+    test_data_loader = DataLoader(test_data, batch_size=batch_size)
+
+    preds_test, t_total = predict(model, test_data_loader, cuda)
+    print("Prediction time per test sample: {:.6f} ms".format(t_total / len(labels_test) * 1000))
+
+    preds_test_unnorm = unnormalize_labels(preds_test, min_val, max_val)
+    print("\nQ-Error for workload '{}':".format(workload_name))
+    print_qerror(preds_test_unnorm, label)
 
 def main():
     parser = argparse.ArgumentParser()
